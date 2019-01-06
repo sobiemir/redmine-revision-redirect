81cfcb84f7a84146660ff0240a3a4e2f99f296e7
sobiemir <sobiemir@aculo.pl>
2019-01-06 15:54:38 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,9 @@
+Redmine::Plugin.register :redmine_revision_redirect do
+  name 'Redmine Revision Redirect plugin'
+  author 'sobiemir'
+  description 'This plugin redirects all revisions to external service 
+with git repository.'
+  version '0.0.1'
+  #url 'http://example.com/path/to/plugin'
+  #author_url 'http://example.com/about'
+end
